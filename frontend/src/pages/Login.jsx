d98e5ace6d3cb23495e8847b295a029af4414ce3
iyan5221 <156579711+iyan5221@users.noreply.github.com>
2025-04-21 15:48:38 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ function Login() {
     setError(''); // clear previous errors
     console.log('Submitting login for:', email);
     try {
-      const res = await fetch('http://172.31.90.56:5000/api/auth/login', {
+      const res = await fetch('http://localhost:5000/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
